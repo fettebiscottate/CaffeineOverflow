6e40ce4a98da529c0521266ab18a616912bf38f2
Giacomo Minello <minellogiacomo@gmail.com>
2019-07-04 16:25:27 +0000

    crea domanda

--- src/com/caffeine/overflow/server/DataBase.java
+++ src/com/caffeine/overflow/server/DataBase.java
@@ -29,6 +29,24 @@ public class DataBase {
 		return DBMaker.newFileDB(new File(DATABASENAME)).make();
 	}
 
+    public static boolean createQuestion(Question question) {
+
+		final DB dataBase = getDB();
+		final BTreeMap<Integer, Question> questions = dataBase.getTreeMap(QUESTIONS);
+		switch (question.getText().length()) {
+		case 0:
+			return false;
+		default:
+			final int id = setQuestionId();
+			question.setIdQuestion(id);
+			questions.put(question.getIdQuestion(), question);
+			dataBase.commit();
+			dataBase.close();
+			return true;
+		}
+
+	}
+	
 	private DataBase() {
 		throw new IllegalStateException("Utility class");
 	}
